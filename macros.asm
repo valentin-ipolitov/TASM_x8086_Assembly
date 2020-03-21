ed38549c4008a5778620c3e4f888f2b20568d78a
Valentyn Ipolitov <sv.vartey@gmail.com>
2020-03-22 01:28:22 +0200

    parametrs filename works

--- macros.asm
+++ macros.asm
@@ -8,5 +8,27 @@ print MACRO msg
       int 21h
       endm
 
+reverseString MACRO params        
+      mov cx, 0
+      mov si offset params
+      pushArgs2:
+            mov dl, [si]
+            inc si
+            inc cx
+            push dx   
+            cmp dx, '$'
+            jne pushArgs2
+
+
+      ;pop
+      mov di, offset params
+      popArgs2:
+            pop dx
+            mov [di], dx
+            inc di
+            loop popArgs2
+endm
+
       
+       
 ; ----------------------------------------------------------------------------------------------------------------------
